8cebe4d31b7e94202793deaa1513c4918ab3aadc
Alexandr <a.kolinko1@gmail.com>
2020-09-25 18:22:53 +0300

    Initial Commit

---
+++ CatFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatFacts
+//
+//  Created by Admin on 25.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
